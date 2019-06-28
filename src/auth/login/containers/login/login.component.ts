b637e8dca6b4bb929d83181108a25e66f913198c
Ricardo Rojas <reddrum7@gmail.com>
2019-06-28 16:46:28 -0500

    [Feat] Create Register & Login form layout

--- src/auth/login/containers/login/login.component.ts
+++ src/auth/login/containers/login/login.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'login',
     template: `
         <div>
           Login
-          <auth-form>
+          <auth-form (submitted)="loginUser($event)">
+            <h1>Login</h1>
+            <a routerLink="/auth/register">Not Registered?</a>
+            <button type="submit">
+              Login
+            </button>
           </auth-form>
         </div>
     `
@@ -15,4 +21,9 @@ export class LoginComponent implements OnInit {
     constructor() { }
 
     ngOnInit() { }
+
+    loginUser(event: FormGroup) {
+      console.log(event.value);
+
+    }
 }
--- src/auth/register/containers/register/register.component.ts
+++ src/auth/register/containers/register/register.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'register',
     template: `
         <div>
           Register
-          <auth-form>
+          <auth-form (submitted)="registerUser($event)">
+            <h1>Register</h1>
+            <a routerLink="/auth/login">Already have an account?</a>
+            <button type="submit">
+              Create account
+            </button>
           </auth-form>
         </div>
     `
@@ -15,4 +21,9 @@ export class RegisterComponent implements OnInit {
     constructor() { }
 
     ngOnInit() { }
+
+    registerUser(event: FormGroup) {
+      console.log(event.value);
+
+    }
 }
