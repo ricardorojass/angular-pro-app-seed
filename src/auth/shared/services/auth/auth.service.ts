b08123b7dc9dfbf5769b198fedbd1fb3df667cc6
Ricardo Rojas <reddrum7@gmail.com>
2019-07-02 11:12:49 -0500

    [Feat] Add data layer and initiate Observables streams

--- src/auth/shared/services/auth/auth.service.ts
+++ src/auth/shared/services/auth/auth.service.ts
@@ -38,6 +38,10 @@ export class AuthService {
     return this.af.authState;
   }
 
+  get user() {
+    return this.af.auth.currentUser;
+  }
+
   createUser(email: string, password: string) {
     return this.af.auth
       .createUserWithEmailAndPassword(email, password);
