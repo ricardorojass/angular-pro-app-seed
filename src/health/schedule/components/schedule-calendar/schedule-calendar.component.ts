46cf667076ec365edc707f4efae4d8422371b1a3
Ricardo Rojas <reddrum7@gmail.com>
2019-07-09 15:56:06 -0500

    [Feat] Render schedule sections from Observables

--- src/health/schedule/components/schedule-calendar/schedule-calendar.component.ts
+++ src/health/schedule/components/schedule-calendar/schedule-calendar.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, Output, EventEmitter, OnChanges } from "@angular/core";
 
+import { ScheduleItem, ScheduleList } from '../../../shared/services/schedule/schedule.service';
+
 @Component({
   selector: 'schedule-calendar',
   styleUrls: ['schedule-calendar.component.scss'],
@@ -15,6 +17,12 @@ import { Component, Input, Output, EventEmitter, OnChanges } from "@angular/core
         [selected]="selectedDayIndex"
         (select)="selectDay($event)">
       </schedule-days>
+
+      <schedule-section
+        *ngFor="let section of sections"
+        [name]="section.name"
+        [section]="getSection(section.key)">
+      </schedule-section>
     </div>
   `
 })
@@ -24,10 +32,20 @@ export class ScheduleCalendarComponent implements OnChanges {
   selectedDay: Date;
   selectedWeek: Date;
 
+  sections = [
+    { key: 'morning', name: 'Morning' },
+    { key: 'lunch', name: 'Lunch' },
+    { key: 'evening', name: 'Evening' },
+    { key: 'snacks', name: 'Snacks and Drinks' },
+  ]
+
   @Input()
   set date(date: Date) {
     this.selectedDay = new Date(date.getTime());
   }
+
+  @Input()
+  items: ScheduleList;
 
   @Output()
   change = new EventEmitter<Date>();
@@ -37,6 +55,10 @@ export class ScheduleCalendarComponent implements OnChanges {
   ngOnChanges() {
     this.selectedDayIndex = this.getToday(this.selectedDay);
     this.selectedWeek = this.getStartOfWeek(new Date(this.selectedDay))
+  }
+
+  getSection(name: string): ScheduleItem {
+    return this.items && this.items[name] || {};
   }
 
   selectDay(index: number) {
